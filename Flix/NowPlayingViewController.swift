d30cc8b4c0b7504e121f72ece453809a49b8486a
user143252 <user143252@74-80-245-31.ic2net.net>
2018-07-31 17:17:21 -0700

    Accessed Movie Titles with JSON

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by user143252 on 7/31/18.
+//  Copyright © 2018 notserpp. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when the network request returns
+            if let error = error{
+            print(error.localizedDescription)
+            }
+        else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+            
+        }
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
